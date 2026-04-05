7cfbeb74ade1f10f1edb3322692efbe11b719cfa
mirao081 <chineduarize4@gmail.com>
2026-04-05 14:08:43 +0100

    Adjusted screen size and responsive layout

--- portfolio.js
+++ portfolio.js
@@ -39,3 +39,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
